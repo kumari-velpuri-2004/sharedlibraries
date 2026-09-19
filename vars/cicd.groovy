889d0297c2337895069efc110334fc8353542aaf
kumari-velpuri-2004 <kumarivelpuri3162@gmail.com>
2026-09-19 13:12:21 +0530

    cicd

---
+++ vars/cicd.groovy
@@ -0,0 +1,20 @@
+def gitDownload(repo)
+{
+  git "https://github.com/IntelliqDevops/${repo}.git"
+}
+
+def buildArtifact()
+{
+  sh 'mvn package'
+}
+
+def deployTomcat(jobname,ip,context)
+{
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
+}
+
+def runSelenium(jobname)
+{
+  sh "scp /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
+
